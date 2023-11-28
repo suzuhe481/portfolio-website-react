fda40122028afc37ff961222fc13c4d8a96da01b
Hector <baseballhector@gmail.com>
2023-11-28 17:29:50 -0500

    Moved App file

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,16 @@
+// import "./App.css";
+import "./App.scss";
+
+import Navbar from "../../components/Navbar/Navbar";
+import Intro from "../../components/Sections/Intro";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Intro />
+    </>
+  );
+}
+
+export default App;
