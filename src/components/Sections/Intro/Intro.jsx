045f85e47737b1921342aa1a4d6c540850c56a85
Hector <baseballhector@gmail.com>
2023-12-03 20:27:20 -0500

    Intro component

---
+++ src/components/Sections/Intro/Intro.jsx
@@ -0,0 +1,17 @@
+import "./Intro.scss";
+
+const Intro = () => {
+  return (
+    <main>
+      <section className="intro">
+        <h2>Hi! My name is</h2>
+        <div className="intro__name">
+          <h1>Hector Suazo</h1>
+        </div>
+        <h2>Software Developer</h2>
+      </section>
+    </main>
+  );
+};
+
+export default Intro;
