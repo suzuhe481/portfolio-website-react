f3b8dd7f90469f5629536668b6dd9be9c5492691
Hector <baseballhector@gmail.com>
2023-12-05 20:03:22 -0500

    Imported more icons. Added color to data.

--- src/components/Sections/About/Technologies/Technologies.jsx
+++ src/components/Sections/About/Technologies/Technologies.jsx
@@ -4,13 +4,30 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHtml5 } from "@fortawesome/free-brands-svg-icons";
 import { faCss3 } from "@fortawesome/free-brands-svg-icons";
 import { faJs } from "@fortawesome/free-brands-svg-icons";
+import { faSass } from "@fortawesome/free-brands-svg-icons";
+import { faGitAlt } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faReact } from "@fortawesome/free-brands-svg-icons";
+import { faPython } from "@fortawesome/free-brands-svg-icons";
+import { faDatabase } from "@fortawesome/free-solid-svg-icons";
+// import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { faNpm } from "@fortawesome/free-brands-svg-icons";
+import { faLinux } from "@fortawesome/free-brands-svg-icons";
 
 import "./Technologies.scss";
 
 const techData = [
-  { name: "HTML", icon: faHtml5 },
-  { name: "CSS", icon: faCss3 },
-  { name: "JavaScript", icon: faJs },
+  { name: "HTML", icon: faHtml5, color: "#f06529" },
+  { name: "CSS", icon: faCss3, color: "#2965f1" },
+  { name: "JavaScript", icon: faJs, color: "#f0db4f" },
+  { name: "SCSS", icon: faSass, color: "#cc6699" },
+  { name: "Git", icon: faGitAlt, color: "#F1502F" },
+  { name: "GitHub", icon: faGithub, color: "#707070" },
+  { name: "React", icon: faReact, color: "#00d8ff" },
+  { name: "Python", icon: faPython, color: "#ffde57" },
+  { name: "SQL", icon: faDatabase },
+  { name: "npm", icon: faNpm, color: "#cb3837" },
+  { name: "Linux", icon: faLinux, color: "#ee7f53" },
 ];
 
 const TechCards = techData.map((tech) => {
@@ -19,6 +36,7 @@ const TechCards = techData.map((tech) => {
       <FontAwesomeIcon
         icon={tech.icon}
         className="technologies-container__card__icon"
+        style={{ color: tech.color }}
       />
       <div className="technologies-container__card__name">{tech.name}</div>
     </div>
