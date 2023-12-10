2e0194257c8f5e7ef11057bd890082832e3f7fd5
Hector <baseballhector@gmail.com>
2023-12-10 16:03:05 -0500

    Including Contact section

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "../../components/Navbar/Navbar";
 import Intro from "../../components/Sections/Intro/Intro";
 import About from "../../components/Sections/About/About";
 import ProjectsSection from "../../components/Sections/ProjectsSection/ProjectsSection";
+import Contact from "../../components/Sections/Contact/Contact";
 
 import "./App.scss";
 
@@ -12,6 +13,7 @@ function App() {
       <Intro />
       <About />
       <ProjectsSection />
+      <Contact />
     </div>
   );
 }
