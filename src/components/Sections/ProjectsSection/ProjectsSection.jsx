ef3bf9936f027f13ac73a9f9d020484db31f0be2
Hector <baseballhector@gmail.com>
2024-11-15 18:12:26 -0500

    Added and rearranged projects

--- src/components/Sections/ProjectsSection/ProjectsSection.jsx
+++ src/components/Sections/ProjectsSection/ProjectsSection.jsx
@@ -7,24 +7,29 @@ import "./ProjectsSection.scss";
 // Array of projects containing project's name, live demo link, and GitHub link
 const projectsData = [
   {
+    title: "Blogga",
+    demo: "https://www.myblogga.com/",
+    github: "https://github.com/suzuhe481/blogga-frontend",
+  },
+  {
+    title: "Typeshift Game",
+    demo: "https://typeshift.hectorsuazo.com/",
+    github: "https://github.com/suzuhe481/typeshift",
+  },
+  {
+    title: "Video Game Inventory",
+    demo: "https://video-game-inventory.glitch.me/inventory",
+    github: "https://github.com/suzuhe481/inventory-management",
+  },
+  {
+    title: "Secret Posts",
+    demo: "secret-posts.glitch.me",
+    github: "https://github.com/suzuhe481/secret-posts",
+  },
+  {
     title: "Battleship",
     demo: "https://battleship.hectorsuazo.com/",
     github: "https://github.com/suzuhe481/battleship",
-  },
-  {
-    title: "CV App Creator",
-    demo: "https://cv-app.hectorsuazo.com/",
-    github: "https://github.com/suzuhe481/cv-app-creator",
-  },
-  {
-    title: "Japanese Cuisine Memory Game",
-    demo: "https://cuisine-game.hectorsuazo.com",
-    github: "https://github.com/suzuhe481/japanese-cuisine-memory-match-game",
-  },
-  {
-    title: "Yard Sale Shopping Cart",
-    demo: "https://yardsale.hectorsuazo.com",
-    github: "https://github.com/suzuhe481/yard-sale-shopping-cart",
   },
   {
     title: "Documentation: React Quick Start Guide",
@@ -36,15 +41,21 @@ const projectsData = [
     demo: "https://github.com/suzuhe481/node-express-quick-start-guide",
     github: "https://github.com/suzuhe481/node-express-quick-start-guide",
   },
+
   {
-    title: "Video Game Inventory",
-    demo: "https://video-game-inventory.glitch.me/inventory",
-    github: "https://github.com/suzuhe481/inventory-management",
+    title: "Japanese Cuisine Memory Game",
+    demo: "https://cuisine-game.hectorsuazo.com",
+    github: "https://github.com/suzuhe481/japanese-cuisine-memory-match-game",
   },
   {
-    title: "Typeshift Game",
-    demo: "https://typeshift.hectorsuazo.com/",
-    github: "https://github.com/suzuhe481/typeshift",
+    title: "Yard Sale Shopping Cart",
+    demo: "https://yardsale.hectorsuazo.com",
+    github: "https://github.com/suzuhe481/yard-sale-shopping-cart",
+  },
+  {
+    title: "CV App Creator",
+    demo: "https://cv-app.hectorsuazo.com/",
+    github: "https://github.com/suzuhe481/cv-app-creator",
   },
 ];
 
